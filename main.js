e60c0ff0eed4580fabf63d8454e62db8ede6af5d
Adi Pascu <mail@adipascu.ro>
2018-03-28 18:04:30 +0300

    Add nodejs project

---
+++ main.js
@@ -0,0 +1,6 @@
+export default ({ getState }) => next => (action) => {
+    console.log('dispatching', JSON.stringify(action, null, '\t'));
+    const result = next(action);
+    console.log('next state', JSON.stringify(getState(), null, '\t'));
+    return result;
+  };
