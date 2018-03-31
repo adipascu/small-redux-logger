f36ded25c2ab71e434f630bfd7928261eaa4bb55
Adi Pascu <mail@adipascu.ro>
2018-03-31 15:27:58 +0300

    Use node exports instead of ES6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-export default ({ getState }) => next => (action) => {
+module.exports =  ({ getState }) => next => (action) => {
     console.log('dispatching', JSON.stringify(action, null, '\t'));
     const result = next(action);
     console.log('next state', JSON.stringify(getState(), null, '\t'));
