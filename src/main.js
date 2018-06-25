d8842072f83ba781e5c9dd3542f329fc1318ad2a
Adi Pascu <adrian@techremix.co>
2018-06-25 12:07:42 +0300

    Use ES6 export

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-module.exports =  ({ getState }) => next => (action) => {
+export default ({ getState }) => next => (action) => {
   console.log('dispatching', JSON.stringify(action, null, '\t'));
   const result = next(action);
   console.log('next state', JSON.stringify(getState(), null, '\t'));
