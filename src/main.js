1f706897c1b58e3abfb3a110fe9d538dc630a95a
Adi Pascu <adrian@techremix.co>
2018-07-24 16:25:35 +0300

    Update logged text

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 export default ({ logActions = true, logState = true } = {}) =>
   ({ getState }) => next => (action) => {
     if (logActions) {
-      console.log('dispatching', JSON.stringify(action, null, '\t'));
+      console.log('Dispatching', JSON.stringify(action, null, '\t'));
     }
     const result = next(action);
     if (logState) {
-      console.log('next state', JSON.stringify(getState(), null, '\t'));
+      console.log('Next state', JSON.stringify(getState(), null, '\t'));
     }
     return result;
   };
