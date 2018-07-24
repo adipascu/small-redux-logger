7c3d849ff77393775494019c572bd8274e27d770
Adi Pascu <adrian@techremix.co>
2018-07-24 16:26:14 +0300

    Remove unused file

--- main.js
+++
@@ -1,6 +0,0 @@
-module.exports = ({ getState }) => next => (action) => {
-  console.log('dispatching', JSON.stringify(action, null, '\t'));
-  const result = next(action);
-  console.log('next state', JSON.stringify(getState(), null, '\t'));
-  return result;
-};
